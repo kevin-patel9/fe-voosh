30fe49a9cc052752936bc970e8b7aac389400c45
Kevin Patel <112944464+kevin-patel9@users.noreply.github.com>
2024-07-25 13:06:16 +0530

    fixed trigger after logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
     const deleteToken = () => {
         localStorage.removeItem("token");
         setIsLogged("");
+        setIsLoggedIn(false);
     };
 
     useEffect(() => {
